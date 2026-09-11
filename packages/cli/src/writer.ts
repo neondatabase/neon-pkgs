0a19bdca39866cc6a9cca64644c9279ffbd2a188
Andre Landgraf <andre.timo.landgraf@gmail.com>
2026-09-10 18:17:17 -0700

    End neon --output json with a newline (#535)

--- packages/cli/src/writer.ts
+++ packages/cli/src/writer.ts
@@ -40,7 +40,7 @@ const writeYaml = (chunks: Chunk[]) => {
 };
 
 const writeJson = (chunks: Chunk[]) => {
-	return JSON.stringify(
+	const json = JSON.stringify(
 		chunks.length === 1
 			? chunks[0].data
 			: Object.fromEntries(
@@ -52,6 +52,7 @@ const writeJson = (chunks: Chunk[]) => {
 		null,
 		2,
 	);
+	return json === undefined ? json : `${json}\n`;
 };
 
 const writeTable = (
